4bf7e65e766b691fe828071eedb4a439feee76c8
Sourav Barua <souravbarua789bd@gmail.com>
2026-01-18 20:37:29 +0600

    implemented auth logic using context

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,60 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  onAuthStateChanged,
+  sendEmailVerification,
+} from "firebase/auth";
+
+
+export const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const register = async (name, email, password) => {
+        // Registration logic will go here
+        const result = await createUserWithEmailAndPassword(auth, email, password);
+        // updating user profile with name
+        await updateProfile(result.user, { displayName: name });
+
+        await sendEmailVerification(result.user);
+
+        return result.user;
+
+    }
+
+    const login = async (email, password) => {
+        return await signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = async () => await signOut(auth);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        }); 
+        return () => unsubscribe();
+    }, []);
+
+    const authData = {
+        user,
+        loading,
+        register,
+        login,
+        logOut,
+    }
+    return (
+        <AuthContext.Provider value={authData}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+}
