75b5027fafbb5e88af3789ea1ee20dcc75da25c8
Sourav Barua <souravbarua789bd@gmail.com>
2026-01-22 21:08:45 +0600

    implemented logout logic

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,58 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router';
-import user from "../assets/user.png"
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router";
+import userIcon from "../assets/user.png";
+import { useAuth } from "../contexts/AuthContext";
 
 const NavBar = () => {
-    return (
-        <div className='mt-5 flex justify-between  items-center w-full relative'>
-            <div></div>
-            <div className='flex gap-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-                <NavLink className="text-accent" to="/">Home</NavLink>
-                <NavLink className="text-accent" to="/about">About</NavLink>
-                <NavLink className="text-accent" to="/career">Career</NavLink>
-            </div>
-            <div className='flex items-center gap-2'>
-                <img src={user} className='h-10 w-10' alt="user" />
-                <Link to="/auth/login" className='btn btn-primary text-white'>Login</Link>
-            </div>
-        </div>
-    );
+  const { user, logOut } = useAuth();
+  const [onProcessing, setOnProcessing] = useState(false);
+  console.log(user);
+  const handleLogout = async () => {
+    setOnProcessing(true)
+    try{
+        await logOut();
+    } catch (error) {
+        alert("Failed to LogOut. Please try again later", error)
+    } finally{
+        setOnProcessing(false)
+    }
+  }
+  return (
+    <div className="mt-5 flex justify-between  items-center w-full relative">
+      <div>
+        {user ? (
+          <>
+            <p className="text-2xl font-medium">{user.displayName}</p>
+            <span className="text-accent">{user.email}</span>
+          </>
+        ) : null}
+      </div>
+      <div className="flex gap-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <NavLink className="text-accent" to="/">
+          Home
+        </NavLink>
+        <NavLink className="text-accent" to="/about">
+          About
+        </NavLink>
+        <NavLink className="text-accent" to="/career">
+          Career
+        </NavLink>
+      </div>
+      <div className="flex items-center gap-2">
+        <img src={userIcon} className="h-10 w-10" alt="user" />
+        {user ? (
+          <button 
+          className="btn btn-primary"
+          onClick={handleLogout}
+          disabled={onProcessing}>LogOut</button>
+        ) : (
+          <Link to="/auth/login" className="btn btn-primary text-white">
+            Login
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
